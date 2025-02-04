ea8c1a2c860bb869c6cbf14100a1f472010b9dd6
Rafał Kołucki <BluRaf@gmx.com>
2025-02-04 22:14:59 +0100

    Format main.cc

--- main.cc
+++ main.cc
@@ -1,32 +1,51 @@
+#include <cstdlib>
+#include <iostream>
 #include <unistd.h>
 
-#include "build.h"
 #include "Logger.h"
 #include "MADServer.h"
+#include "build.h"
 
+#define DEFAULT_CONFIG_FILE "~/.hyperion/Shogo/ShogoSrv.cfg"
+
+void usage(std::string progname)
+{
+    std::clog << "Usage: " << progname << " [-e] [-c config] [-v]" << std::endl;
+    std::clog << "\t-e\t\tStop server after all players leave the game" << std::endl;
+    std::clog << "\t-c config\tLoad config file (default: " << DEFAULT_CONFIG_FILE << ")" << std::endl;
+    std::clog << "\t-v\t\tPrint the version and quit" << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
     bool exit_when_empty = false;
-    std::string config_file = "ShogoServ.cpp";
+    bool only_print_version = false;
+    std::string config_file = DEFAULT_CONFIG_FILE;
 
     char c;
-    while ((c = getopt (argc, argv, "c:e")) != -1) {
-        switch (c) {
-            case 'c':
-                config_file = std::string(optarg);
-                break;
-            case 'e':
-                exit_when_empty = true;
-                break;
-            default:
-                std::clog << "usage: " << argv[0] << " [-e] [-c ShogoServ.cfg]" << std::endl;
-                return EXIT_FAILURE;
+    while ((c = getopt(argc, argv, "c:ev")) != -1)
+    {
+        switch (c)
+        {
+        case 'c':
+            config_file = std::string(optarg);
+            break;
+        case 'e':
+            exit_when_empty = true;
+            break;
+        case 'v':
+            only_print_version = true;
+            break;
+        default:
+            usage(argv[0]);
+            return EXIT_FAILURE;
         }
     }
 
     LOG_INFO << "MADServer " << BUILD_REVISION;
     LOG_INFO << "Copyright (c) 2025, BluRaf / Team Zlomformatyk";
+    if (only_print_version)
+        return 0;
 
     MADServer server;
     LOG_INFO << "Server is ready!";
